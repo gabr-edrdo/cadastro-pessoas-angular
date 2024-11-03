732e6a23550032b520740187a864dfbfc5380051
Gabriel Araujo <gabr.edrdo@gmail.com>
2024-11-03 17:58:15 -0300

    Add componentes

---
+++ src/app/components/pessoa-list/pessoa-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pessoa-list.component.html',
+  styleUrl: './pessoa-list.component.scss'
+})
+export class PessoaListComponent {
+
+}
